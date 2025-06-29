5c299ccfc3787ca6b9aebb6b59c7e4356547c21a
Paulo de Pinho <prdepinho@gmail.com>
2025-06-29 15:59:23 -0300

    gitignore

--- build/conandeps_legacy.cmake
+++
@@ -1,9 +0,0 @@
-message(STATUS "Conan: Using CMakeDeps conandeps_legacy.cmake aggregator via include()")
-message(STATUS "Conan: It is recommended to use explicit find_package() per dependency instead")
-
-find_package(Poco)
-find_package(Catch2)
-find_package(OpenSSL)
-find_package(ZLIB)
-
-set(CONANDEPS_LEGACY  Poco::Poco  Catch2::Catch2WithMain  openssl::openssl  ZLIB::ZLIB )
